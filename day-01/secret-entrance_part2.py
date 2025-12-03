8474319ff9121112108ab700fbe693c825207aff
Juan Gacía <juan.garcia.anton@gmail.com>
2025-12-03 23:21:45 +0100

    reestructuradas las carpetas

--- day-01/secret-entrance_part2.py
+++ day-01/secret-entrance_part2.py
@@ -12,6 +12,7 @@ def procesar_texto(texto_entrada):
             lista = texto_entrada.split()
 
             for paso in lista:
+                vueltas = 0
                 paso = paso.replace(",", "").replace(".", "").strip()
 
                 if not paso: continue
@@ -19,29 +20,21 @@ def procesar_texto(texto_entrada):
                 letra = paso[0].upper()
                 numero = int(paso[1:])
 
-                if letra == "R":
-                    # Girar a la derecha: de dial a (dial + numero)
-                    # Contamos cuántas veces pasamos por 0
-                    # Si dial=50 y numero=60, vamos de 50 a 110, pasamos por 100 (que es 0)
-                    veces_por_cero = (dial + numero) // 100
-                    dial = (dial + numero) % 100
-                    
-                elif letra == "L":
-                    # Girar a la izquierda: de dial a (dial - numero)
-                    # Si dial=50 y numero=68, vamos de 50 hacia abajo 68 clicks
-                    # Pasamos por 0 cuando dial - clicks = 0, es decir cuando clicks = dial
-                    # Entonces pasamos por 0 cada 100 clicks después del primer 0
-                    # Número de veces que pasamos por 0 = (numero + (100 - dial)) // 100
-                    if dial == 0 and numero % 100 == 0:
-                        veces_por_cero = (numero // 100)
-                    elif numero > dial:
-                        veces_por_cero = 1 + (numero - dial) // 100
-                    else:
-                        veces_por_cero = 0
-                    dial = (dial - numero) % 100
                 
-                print(f"Paso: {paso}, Dial ahora: {dial}, Veces por cero: {veces_por_cero}")
-                contador += veces_por_cero
+                #mal tengo que revisar los ceros intermedios, por ejemplo si tengo R200 mi codigo devuelve 2, pero tendría
+                if letra == "R":
+                    if ((numero+dial) %100) != 0: vueltas = (numero + dial) // 100
+                    dial = (dial + numero) % 100
+
+                elif letra == "L":
+                    if ((numero-dial) %100) != 0: vueltas = (numero + dial) // 100
+                    dial = (dial - numero) % 100
+            
+                if dial%100 == 0:
+                    print(f"El acumulador volvió a cero con el paso: {paso}")
+                    contador += 1
+                contador += vueltas
+
 
         return contador
     except FileNotFoundError:
