f53f58c0acf6880d3ce17d15360b206cd75fb885
Juan Gacía <juan.garcia.anton@gmail.com>
2025-12-03 12:11:49 +0100

    Finalizado dia 1 parte 1

---
+++ day-01/secret-entrance/README.md
@@ -0,0 +1,2 @@
+Parte 1 -> https://adventofcode.com/2025/day/1
+Parte 2 -> https://adventofcode.com/2025/day/1#part2
---
+++ day-01/secret-entrance/secret-entrance.py
@@ -0,0 +1,49 @@
+import sys
+
+def procesar_texto(texto_entrada):
+    dial = 50
+    contador = 0
+
+    try:
+        with open(texto_entrada, 'r', encoding='utf-8') as archivo:
+            #.read lee todo el archivo como un solo string
+            texto_entrada = archivo.read()
+            #uso split para separar cortar el texto por espacios, saltos de línea, tabulaciones (en este caso saltos de línea)
+            lista = texto_entrada.split()
+
+            for paso in lista:
+                paso = paso.replace(",", "").replace(".", "").strip()
+
+                if not paso: continue
+
+                letra = paso[0].upper()
+                numero = int(paso[1:])
+
+                if letra == "R":
+                    dial = (dial + numero) % 100
+                elif letra == "L":
+                    dial = (dial - numero) % 100
+                
+                if dial%100 == 0:
+                    print(f"El acumulador volvió a cero con el paso: {paso}")
+                    contador += 1
+
+        return contador
+    except FileNotFoundError:
+        print(f"Error: El archivo '{texto_entrada}' no se encontró.")
+        return None
+    except ValueError:
+        print("Error: El archivo contiene datos no válidos.")
+        return None
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Uso: python secret-entrance.py <ruta_del_archivo>")
+        sys.exit(1)
+
+    ruta_archivo = sys.argv[1]
+    resultado = procesar_texto(ruta_archivo)
+
+    if resultado is not None:
+        print(f"Número de veces que el acumulador vuelve a cero: {resultado}")
+                
+    
