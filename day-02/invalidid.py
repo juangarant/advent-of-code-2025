5f47d80336d2ff8662b880a67043068fc222a42d
Juan Gacía <juan.garcia.anton@gmail.com>
2025-12-04 11:52:53 +0100

    Compelta dia 2 parte 1

--- day-02/invalidid.py
+++ day-02/invalidid.py
@@ -1,14 +1,29 @@
 import sys
 
+def is_repetition_list(seq):
+    n = len(seq)
+    if n%2 != 0:
+        return False, None
+        
+    mitad = n // 2
+
+    return seq[:mitad] == seq[mitad:], None
+
 def validate_id(path):
     try:
         with open(path, 'r', encoding='utf-8') as archivo:
             inicialarray = archivo.read().split(",")
-
+            total = 0
             for element in inicialarray:
                 actualelement = element.split("-")
                 actualarray = list(range(int(actualelement[0]), int(actualelement[1])+1))
-            return 0
+                for i in actualarray:
+                    realactualarray = list(map(int, str(i)))
+                    if is_repetition_list(realactualarray)[0]:
+                        print(f"El ID {i} es inválido debido a repeticiones.")
+                        total += i
+                
+            return total
 
     except FileNotFoundError:
         print(f"Error: El archivo '{id}' no se encontró.")
@@ -21,3 +36,5 @@ if __name__ == "__main__":
 
     path = sys.argv[1]
     es_valido = validate_id(path)
+    if es_valido is not False:
+        print(f"Suma de IDs inválidos: {es_valido}")
